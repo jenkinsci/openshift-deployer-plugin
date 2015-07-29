74c57a5f6d2626abd5d14bbc9daca56e3c312f6f
Julien Piccaluga <julien.piccaluga@nagra.com>
2015-07-29 17:47:24 +0200

    JENKINS-29700 Backslash should escape equal character when evaluation environment variables

--- src/main/java/org/jenkinsci/plugins/openshift/DeployApplication.java
+++ src/main/java/org/jenkinsci/plugins/openshift/DeployApplication.java
@@ -142,7 +142,9 @@ public class DeployApplication extends Builder implements BuildStep {
 				Map<String, String> mapOfEnvironmentVariables = new HashMap<String, String>();
 				for (String environmentVariable : Arrays.asList(environmentVariables.split(" "))) {
 					if (environmentVariable.contains("=")) {
-						String[] parts = environmentVariable.split("=");
+                        // regex do not split equal escaped by a backslash
+                        String regex = ".*[^\\\\]=.*";
+						String[] parts = environmentVariable.split(regex);
 						mapOfEnvironmentVariables.put(parts[0], parts[1]);
 					} else {
 						abort(listener, "Invalid environment variable: " + environmentVariable);
